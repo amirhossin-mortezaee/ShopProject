7370103cbe4ec193a675862e8f1ba251a9df95b7
amirhossin-mortezaee <amirhossinprog@gmail.com>
2023-07-12 13:46:46 +0330

    ایجاد پنل ادمین اسلایدر واضافه کردن دیتابیس

---
+++ shopProject/shopProject/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectShopCMS.DAL;
+
+namespace shopProject.Areas.Admin.Controllers
+{
+    public class SlidersController : Controller
+    {
+        private projectShopDBEntities db = new projectShopDBEntities();
+
+        // GET: Admin/Sliders
+        public ActionResult Index()
+        {
+            return View(db.Slider.ToList());
+        }
+
+        // GET: Admin/Sliders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Sliders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SliderId,DiscountTitle,Title,ImageName,StartSliderDate,EndSliderDate,IsActive")] Slider slider)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Slider.Add(slider);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // POST: Admin/Sliders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SliderId,DiscountTitle,Title,ImageName,StartSliderDate,EndSliderDate,IsActive")] Slider slider)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(slider).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Slider slider = db.Slider.Find(id);
+            if (slider == null)
+            {
+                return HttpNotFound();
+            }
+            return View(slider);
+        }
+
+        // POST: Admin/Sliders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Slider slider = db.Slider.Find(id);
+            db.Slider.Remove(slider);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
