0700a6ccd4d2be2fa5817f95be3f0d97c4a5e327
amirhossin-mortezaee <amirhossinprog@gmail.com>
2023-07-18 13:45:37 +0330

    ساخت فرم ثبت نام و اعتبار سنجی آن

---
+++ ProjectShopCMS.DAL/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShopCMS.DAL.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "نام کاربر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string UserName { get; set; }
+        [Display(Name = "ایمیل ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage = "لطفا ایمیل معتبر وارد نمایید")]
+        public string Email { get; set; }
+        [Display(Name = "رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "تکرار رمز عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "کلمه های عبور با یکدیگر مطابقت ندارند")]
+        public string RePassword { get; set; }
+    }
+}
---
+++ shopProject/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using ProjectShopCMS.DAL;
+using ProjectShopCMS.DAL.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace shopProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        projectShopDBEntities db = new projectShopDBEntities();
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel register)
+        {
+            if(ModelState.IsValid)
+            {
+                if(!db.User.Any(u => u.Email == register.Email.Trim().ToLower()))
+                {
+                    User user = new User()
+                    {
+                        UserName = register.UserName,
+                        Email = register.Email.Trim().ToLower(),
+                        Password = FormsAuthentication.HashPasswordForStoringInConfigFile(register.Password, "MD5"),
+                        ActivationCode = Guid.NewGuid().ToString(),
+                        IsActive = false,
+                        CreateDate = DateTime.Now,
+                        RoleId = 1
+                    };
+                    db.User.Add(user);
+                    db.SaveChanges();
+                    return View("SuccsessMessageForRegister",user);
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "ایمیل وارد شده تکرار می باشد");
+                }
+            }
+            return View(register);
+        }
+    }
+}
