b0e8a804f9209f8856c2b2c3cb6f9af0a5903388
amirhossin-mortezaee <amirhossinprog@gmail.com>
2023-08-05 14:56:40 +0330

    دسته بندی محصولات سایت

--- ProjectShopCMS.DAL/MetaData/ProductGropsMetaData.cs
+++ ProjectShopCMS.DAL/MetaData/ProductGropsMetaData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,17 @@ namespace ProjectShopCMS.DAL
 {
     class ProductGropsMetaData
     {
+        public int GroupId { get; set; }
+        [Display(Name = "عنوان گروه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public string GroupTitle { get; set; }
+        [Display(Name = "عنوان زیر گروه ها")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public Nullable<int> ParentId { get; set; }
+        [MetadataType(typeof(ProductGropsMetaData))]
+        public partial class ProductGrops
+        {
+
+        }
     }
 }
