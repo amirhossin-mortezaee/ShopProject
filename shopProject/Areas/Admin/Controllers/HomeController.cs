c356f5ac086203eda6f33dc3f890dde006587317
amirhossin-mortezaee <amirhossinprog@gmail.com>
2023-08-13 13:50:35 +0330

    نمایش آخرین محصولات در سایت

---
+++ shopProject/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace shopProject.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("logOFF")]
+        public ActionResult logOFF()
+        {
+            FormsAuthentication.SignOut();
+            return Redirect("/");
+        }
+    }
+}
---
+++ shopProject/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using ProjectShopCMS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        projectShopDBEntities db = new projectShopDBEntities();
+        public ActionResult LastProduct()
+        {
+            var LaSTProduct = db.Products.OrderByDescending(x => x.CreateDate).Take(8); 
+            return PartialView(LaSTProduct);
+        }
+    }
+}
