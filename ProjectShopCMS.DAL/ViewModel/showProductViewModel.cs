9a014b842c6c2e0587157bcc3af85aad14c63781
amirhossin-mortezaee <amirhossinprog@gmail.com>
2023-08-16 11:16:59 +0330

    نمایش جزئیات محصول در سایت

---
+++ ProjectShopCMS.DAL/ViewModel/showProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectShopCMS.DAL.ViewModel
+{
+    public class showProductViewModel
+    {
+        public string FeatureTitle { get; set; }
+        public List<string> Values { get; set; }
+    }
+}
--- shopProject/Controllers/ProductController.cs
+++ shopProject/Controllers/ProductController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjectShopCMS;
+using ProjectShopCMS.DAL.ViewModel;
 
 namespace shopProject.Controllers
 {
@@ -15,6 +17,18 @@ namespace shopProject.Controllers
         {
             var LaSTProduct = db.Products.OrderByDescending(x => x.CreateDate).Take(8); 
             return PartialView(LaSTProduct);
+        }
+        [Route("showProductDetails/{id}")]
+        public ActionResult showProductDetails(int id)
+        {
+            var product = db.Products.Find(id);
+            ViewBag.category = product.SelectedProductCateGory.ToList();
+            ViewBag.ProductFeature = product.ProductFeature.DistinctBy(f => f.FeatureId).Select(f => new showProductViewModel()
+            {
+                FeatureTitle = f.Feature.FetureTitle,
+                Values = product.ProductFeature.Where(fe => fe.FeatureId == f.FeatureId).Select(fe => fe.Value).ToList()
+            }).ToList();
+            return View(product);
         }
     }
 }
