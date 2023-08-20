7cf4b14b19ad9d2ed54e7e7b468b12630d0f2617
amirhossin-mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2023-08-20 16:24:23 +0330

    پیاده سازی جستجو در سایت

---
+++ shopProject/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using ProjectShopCMS.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shopProject.Controllers
+{
+    public class SearchController : Controller
+    {
+        projectShopDBEntities db = new projectShopDBEntities();
+        // GET: Search
+        public ActionResult Index(string q)
+        {
+            List<Products> listSearch = new List<Products>();
+            listSearch.AddRange(db.ProductTags.Where(t => t.Tag == q).Select(t => t.Products).ToList());
+            listSearch.AddRange(db.Products.Where(t => t.ProductTitle.Contains(q)||t.TextProduct.Contains(q)||t.ShortDiscription.Contains(q)).ToList());
+            ViewBag.SearchParameter = q;
+            return View(listSearch.Distinct());
+        }
+    }
+}
