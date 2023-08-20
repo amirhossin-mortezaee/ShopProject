0f9dab1e292e089b13e0b5797051c8d86e698f60
amirhossin-mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2023-08-20 15:31:12 +0330

    نمایش نظرات کاربران برای محصول

--- shopProject/Controllers/ProductController.cs
+++ shopProject/Controllers/ProductController.cs
@@ -15,7 +15,7 @@ namespace shopProject.Controllers
         projectShopDBEntities db = new projectShopDBEntities();
         public ActionResult LastProduct()
         {
-            var LaSTProduct = db.Products.OrderByDescending(x => x.CreateDate).Take(8); 
+            var LaSTProduct = db.Products.OrderByDescending(x => x.CreateDate).Take(8);
             return PartialView(LaSTProduct);
         }
         [Route("showProductDetails/{id}")]
@@ -28,7 +28,33 @@ namespace shopProject.Controllers
                 FeatureTitle = f.Feature.FetureTitle,
                 Values = product.ProductFeature.Where(fe => fe.FeatureId == f.FeatureId).Select(fe => fe.Value).ToList()
             }).ToList();
+            ViewBag.courentcomment = db.ProductComment.Count();
             return View(product);
+        }
+
+        public ActionResult showComments(int id)
+        {
+            var showComment = db.ProductComment.Where(c => c.ProductId == id);
+            return PartialView(showComment);
+        }
+        [HttpGet]
+        public ActionResult CreateComments(int id)
+        
+        {
+            return PartialView(new ProductComment() { ProductId = id });
+        }
+        [HttpPost]
+        public ActionResult CreateComments(ProductComment comments)
+        {
+            if (ModelState.IsValid)
+            {
+                comments.CreateDate = DateTime.Now;
+                db.ProductComment.Add(comments);
+                db.SaveChanges();
+                return PartialView("ShowComments", db.ProductComment.Where(c => c.ProductId == comments.ProductId));
+
+            }
+            return PartialView(comments);
         }
     }
 }
